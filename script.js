e1927e831e5dd10425cc1759e9e3e3b8337f7b94
OwenC525 <525ochiu@frhsd.com>
2024-02-01 10:43:55 -0500

    worked on button and fixed cards and footer

---
+++ script.js
@@ -0,0 +1,6 @@
+function buttonhome(){
+    document.getElementById('imgchange').src = "..."
+    document.getElementById('titlechange').textContent = "Events that happened on my birthday!"
+    document.getElementById('textchange').textContent = "Click on the buttons below!"
+    document.getElementById('imgchange').alt = "home page img"
+}
