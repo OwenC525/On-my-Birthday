c0d8ac8f51b73bfce1c837c6e64b8a3e7088a7c9
Owen Chiu <owencc8@gmail.com>
2024-02-02 07:07:56 -0500

    added images and button js and card text; finished

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
 function buttonhome(){
-    document.getElementById('imgchange').src = "..."
+    document.getElementById('imgchange').src = "images/october10.jpeg"
     document.getElementById('titlechange').textContent = "Events that happened on my birthday!"
     document.getElementById('textchange').textContent = "Click on the buttons below!"
     document.getElementById('imgchange').alt = "home page img"
+}
+
+function button1846(){
+    document.getElementById('imgchange').src = "images/triton.jpeg" // changes image
+    document.getElementById('titlechange').textContent = "Discovery of Triton" // changes title
+    document.getElementById('textchange').textContent = "On October 10, 1846, english astronomer William Lassell discovered Triton, the largest satellite of the planet Neptune." // changes text
+    document.getElementById('imgchange').alt = "discovered Triton image."
+    
+}
+
+function button1911(){
+    document.getElementById('imgchange').src = "images/Hubei_Military_Government.jpg"
+    document.getElementById('titlechange').textContent = "Uprising in Wuchang, China "
+    document.getElementById('textchange').textContent = "On October 10, 1911, A group of revolutionaries launched an uprising in Wuchang, China. This began the Chinese Revolution, a nationalist democratic revolt that overthrew the Qing dynasty."
+    document.getElementById('imgchange').alt = "revolutionaries launched an uprising in Wuchang, China image"
+}
+
+function button1935(){
+    document.getElementById('imgchange').src = "images/porgyandbess.webp"
+    document.getElementById('titlechange').textContent = "Porgy and Bess Premieres"
+    document.getElementById('textchange').textContent = "On October 10, 1935, George Gershwin’s opera Porgy and Bess premieres on Broadway."
+    document.getElementById('imgchange').alt = "opera Porgy and Bess opened on Broadway image"
+}
+
+function button1970(){
+    document.getElementById('imgchange').src = "images/fiji.jpeg"
+    document.getElementById('titlechange').textContent = "Fiji gains independence"
+    document.getElementById('textchange').textContent = "The United States recognized Fiji’s independence on October 10, 1970. Fiji became independent from the British Commonwealth on that date."
+    document.getElementById('imgchange').alt = "Fiji gained independence image."
+}
+
+function button2010(){
+    document.getElementById('imgchange').src = "images/Nt-map.png"
+    document.getElementById('titlechange').textContent = "Netherlands Antilles Dissolved"
+    document.getElementById('textchange').textContent = "The Netherlands Antilles—formerly an autonomous part of the Kingdom of the Netherlands consisting of a group of five islands in the Caribbean Sea—was officially dissolved."
+    document.getElementById('imgchange').alt = "Netherlands dissolved image"
 }
